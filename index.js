dba957ecc08ffb37d4b223b762f8567a42d84802
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-29 16:47:44 -0500

    New route

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 // * Crear servidor de Express
 const app = express();
 
+// * Rutas
+app.get( '/', (req, res) => { // * req: Info de los headers - res: Info que le mostraremos al usuario
+    
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    })
+
+})
+
 // * Escuchar o levantar el servidor 
 app.listen( 3000, () => {   // * Puerto
     console.info('Servidor corriendo en el puerto 3000') // * Mensaje satisfactorio
