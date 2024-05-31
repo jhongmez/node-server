54b80c33a7d5b391a08af377c056bab773cfc9dd
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-31 10:06:47 -0500

    Encrypt password

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,59 +1,63 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const Users = require('../models/user');
 
 const getUsers = async(req, res) => { // * req: Info de los headers - res: Info que le mostraremos al usuario
 	
-    const users = await Users.find();
+	const users = await Users.find();
 
 	res.json({
 		success: true,
-        message: 'Datos obtenidos correctamente',
-        users
+		message: 'Datos obtenidos correctamente',
+		users
 	})
 
 }
 
 const createUser = async(req, res = response) => {
 
-    const { fullname, email, password } = req.body;
+	const { email, password } = req.body;
 
+	try {
+		
+		const existEmail = await Users.findOne({ email });
 
-    try {
-        
-        const existEmail = await Users.findOne({ email });
+		// * Validacion para correos unicos
+		if ( existEmail ) {
+			return res.status(400).json({
+				success: false,
+				message: 'El correo ya está en uso'
+			})
+		}
 
-        // * Validacion para correos unicos
-        if ( existEmail ) 
-            return res.status(400).json({
-                success: false,
-                message: 'El correo ya está en uso'
-            })
+		const user = new Users( req.body );
 
+		// * Encriptar contraseña
+		const salt = bcrypt.genSaltSync(); // * Genera una data aleatoria
+		user.password = bcrypt.hashSync( password, salt )
+	
+		await user.save();
+	
+		res.json({
+			success: true,
+			message: 'Usuario creado correctamente',
+			user // * mostramos el dato enviado
+		})
+		
+	} catch (error) {
 
-        const user = new Users( req.body );
-    
-        await user.save();
-    
-        res.json({
-            success: true,
-            message: 'Usuario creado correctamente',
-            user // * mostramos el dato enviado
-        })
-        
-    } catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: 'ERROR al crear el usuario',
+		});
 
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'ERROR al crear el usuario',
-        });
-
-    }
+	}
 
 
 }
 
 module.exports = {
-    getUsers, 
-    createUser
+	getUsers, 
+	createUser
 }
