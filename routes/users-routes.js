207d482fe514859d6f7be0f765034be3d89a3388
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-31 08:56:14 -0500

    Create middleware request post users

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,15 +1,24 @@
 /*
-  * RUTA: /api/users
+	* RUTA: /api/users
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator')
 const { getUsers, createUser } = require('../controllers/users-controller');
 
 const router = Router();
 
-router.get( '/', getUsers )
+// * Estructura: Ruta, Middleware, Controlador
+router.get( '/', getUsers );
 
-router.post( '/', createUser )
+router.post( '/',
+	[
+		check('fullname', 'El nombre es obligatorio').not().isEmpty(),
+		check('email', 'El email es obligatorio').not().isEmpty(),
+		check('password', 'La contraseña es obligatoria').isEmail(),
+	], 
+	createUser 
+);
 
 
 // * Exportacion del modulo router
