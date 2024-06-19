00d7ec2497f5da0780a03b75993ae4d339002078
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-06-18 19:54:50 -0500

    Validation for email and password

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,14 +1,43 @@
 const { response } = require('express');
-const Auth = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 
 
 const login = async(req, res = response) => {
 
+    const { email, password } = req.body
+
     try {
+
+        // * Busque el usuario por el email
+        const userDB = await User.findOne({ email }); 
+
+        // * Verifica el email
+        if ( !userDB ) {
+            return res.status(404).json({
+                success: false,
+                message: 'ERROR el email no se encontró'
+            })
+        }
+
+        // * Verificar contraseña
+        const validPassword = bcrypt.compareSync( password, userDB.password );
+
+        if( !validPassword ) {
+            return res.status(404).json({
+                success: false,
+                message: 'ERROR el password es invalido'
+            })
+        }
+
+        // * Generar JWT
+
         res.json({
             success: true,
             message: 'Conexion exitosa'
-        })
+        });
+
     } catch (error) {
         console.log(error);
 		res.status(500).json({
