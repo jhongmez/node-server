35e6fb658e2309291f032f0674b9518869458f85
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-08-15 07:47:27 -0500

    Modify controller search

--- controllers/search-controller.js
+++ controllers/search-controller.js
@@ -1,13 +1,27 @@
 const { response } = require('express');
+const User = require('../models/user');
+const Doctor = require('../models/doctors');
+const Hospital = require('../models/hospitals');
 
-const getResults = ( req, res = response ) => {
+const getResults = async( req, res = response ) => {
 
     const search = req.params.busqueda;
+
+    const regexp = new RegExp( search, 'i' )
+
+
+    const [ users, doctors, hospitals ] = await Promise.all([
+        User.find({ fullname: regexp }),
+        Doctor.find({ name: regexp }),
+        Hospital.find({ name: regexp })
+    ])
 
     res.json({
 		success: true,
 		message: 'Datos obtenidos correctamente',
-        search
+        users,
+        doctors,
+        hospitals
 	})
 }
 
