4e5bb666d2c13bf4698712f1139a474091118736
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-08-21 21:04:38 -0500

    ✅ [ADD] Functions for upload images

--- routes/upload-routes.js
+++ routes/upload-routes.js
@@ -2,10 +2,13 @@
 	* RUTA: /api/upload/:busqueda
 */
 const { Router } = require('express');
+const expressFileUpload = require('express-fileupload')
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { uploadFiles } = require('../controllers/upload-controller');
 
 const router = Router();
+
+router.use( expressFileUpload() );
 
 router.put('/:type/:id', uploadFiles)
 
